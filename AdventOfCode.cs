cfed736da4b6f605ee4e06bca62e635723432d23
Mathias Paumgarten <mail@mathias-paumgarten.com>
2016-12-14 11:32:03 -0800

    remove running all days in main

--- AdventOfCode.cs
+++ AdventOfCode.cs
@@ -7,46 +7,12 @@ public static class AdventOfCode {
 
     public static void Main( string[] args ) {
 
-        if ( args.Length > 0 ) {
+        if ( args.Length == 2 ) {
             RunSpecific( Int32.Parse( args[ 0 ] ), Int32.Parse( args[ 1 ] ) );
             return;
         }
 
-        string input;
-
-        input = ReadInput( "day-01/input.txt" );
-
-        PrintResult( 1, 1, Day1.Part1( input ) );
-        PrintResult( 1, 2, Day1.Part2( input ) );
-
-        input = ReadInput( "day-02/input.txt" );
-
-        PrintResult( 2, 1, Day2.Part1( input ) );
-        PrintResult( 2, 2, Day2.Part2( input ) );
-
-        input = ReadInput( "day-03/input.txt" );
-
-        PrintResult( 3, 1, Day3.Part1( input ) );
-        PrintResult( 3, 2, Day3.Part2( input ) );
-
-        input = ReadInput( "day-04/input.txt" );
-
-        PrintResult( 4, 1, Day4.Part1( input ) );
-        PrintResult( 4, 2, Day4.Part2( input ) );
-
-        input = ReadInput( "day-05/input.txt" );
-
-        PrintResult( 5, 1, Day5.Part1( input ) );
-        PrintResult( 5, 2, Day5.Part2( input ) );
-
-        input = ReadInput( "day-06/input.txt" );
-
-        PrintResult( 6, 1, Day6.Part1( input ) );
-        PrintResult( 6, 2, Day6.Part2( input ) );
-
-        input = ReadInput( "day-07/input.txt" );
-
-        PrintResult( 7, 1, Day7.Part1( input ) );
+        Console.WriteLine( "Wrong number of arguments. Two arguments needed: <day> <part>" );
     }
 
     private static void RunSpecific( int day, int part ) {
---
+++ day-09/Day9.cs
@@ -0,0 +1,7 @@
+public static class Day9 {
+
+    public static string Part1( string input ) {
+        return "";
+    }
+
+}
